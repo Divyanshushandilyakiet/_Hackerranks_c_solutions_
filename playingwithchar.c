40e5b25f55ac79078733420484e41e52b73c2e53
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-06-16 22:43:29 +0530

    Create playingwithchar.c

---
+++ playingwithchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+char ch;
+char s[100];
+char san[100];
+
+scanf("%c",&ch);
+scanf("%s\n",s);
+gets(san);
+
+printf("%c\n",ch);
+printf("%s\n",s);
+printf("%s",san);
+
+
+}
