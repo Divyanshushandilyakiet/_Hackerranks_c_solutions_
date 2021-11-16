628f496884321f5890d3ca6d805f474fc51051ba
Divyanshu shandilya <70764631+Divyanshushandilyakiet@users.noreply.github.com>
2021-11-16 21:41:42 +0530

    Create Viraladvertising.c

---
+++ Problemsolving/Viraladvertising.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int i=0,j,k,l,m,n,t;
+    scanf("%d",&n);
+    k=5;
+    for(i=0;i<n;i++)
+        {
+        j=k/2;
+        k=j*3;
+        m=m+j;
+    }
+    printf("%d",m);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
+  
